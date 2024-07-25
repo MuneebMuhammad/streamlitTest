cbe78413ad31b1a12cd2a86d89b5f8cdb7ec632c
Muneeb Muhammad <muneebkattak57@gmail.com>
2024-07-25 14:29:30 +0500

    third

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
-
+import time
 
 def process_text(input_text, model="abc"):
+    time.sleep(5)
     print("using model:", model)
     return f"processed text using model: {model}"
 
 def main():
 
-    st.selectbox("Select LLM:", ['GPT 4o mini', 'Gemini 1.5 Pro'], key='model_name')
+    st.selectbox("tmeSelect LLM:", ['GPT 4o mini', 'Gemini 1.5 Pro'], key='model_name')
     st.selectbox("Select type of law:", ['Family Law', 'Property Law', 'Civil Law', 'Corporate Law'], key='law_type')
     t = st.text_area("Enter text here")
 
